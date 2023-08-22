3aa4f37eb175b83bc4395f932eb63288fa522493
Uuna Saarela <uuna.saarela@gmail.com>
2023-08-22 21:55:34 +0300

    4.4

--- bloglist/tests/list_helper.test.js
+++ bloglist/tests/list_helper.test.js
@@ -1,8 +1,29 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('list_helper', () => {
+  test('dummy returns one', () => {
+    const blogs = []
+  
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+  test('totalLikes returns total likes', () => {
+    const blogs = [
+        {
+            title: "Blog 1", 
+            author: "Author 1", 
+            url: "www.blog1.com", 
+            likes: 4
+        }, 
+        {
+            title: "Blog 2", 
+            author: "Author 1", 
+            url: "www.blog1.com", 
+            likes: 3
+        }, 
+    ]
+    
+    expect(listHelper.totalLikes(blogs)).toBe(7)
+  })
 })
--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+    const likeSum = blogs.map((blog) => blog.likes).reduce((a, b) => a + b, 0)
+    return likeSum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
